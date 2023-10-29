4f1991e330a8c2ccb7790cee1f6d2ac193fa43d5
RJBarker <ryan.barker@intralox.com>
2023-10-29 19:34:28 +0000

    varchar length brackets added

--- group_project/crowdfunding_schema.sql
+++ group_project/crowdfunding_schema.sql
@@ -7,34 +7,34 @@ DROP TABLE "subcategory" CASCADE;
 CREATE TABLE "campaign" (
     "cf_id" int   NOT NULL,
     "contact_id" int   NOT NULL,
-    "company_name" varchar64   NOT NULL,
-    "description" varchar64   NOT NULL,
+    "company_name" varchar(64)   NOT NULL,
+    "description" varchar(64)   NOT NULL,
     "goal" numeric(10)   NOT NULL,
     "pledged" numeric(10)   NOT NULL,
-    "outcome" varchar25   NOT NULL,
+    "outcome" varchar(25)   NOT NULL,
     "backers_count" int   NOT NULL,
-    "country" varchar5   NOT NULL,
-    "currency" varchar5   NOT NULL,
+    "country" varchar(5)   NOT NULL,
+    "currency" varchar(5)   NOT NULL,
     "launched_date" date   NOT NULL,
     "end_date" date   NOT NULL,
-    "category_id" varchar10   NOT NULL,
-    "subcategory_id" varchar10   NOT NULL,
+    "category_id" varchar(10)   NOT NULL,
+    "subcategory_id" varchar(10)   NOT NULL,
     CONSTRAINT "pk_campaign" PRIMARY KEY (
         "contact_id"
      )
 );
 
 CREATE TABLE "category" (
-    "category_id" varchar10   NOT NULL,
-    "category" varchar64   NOT NULL,
+    "category_id" varchar(10)   NOT NULL,
+    "category" varchar(64)   NOT NULL,
     CONSTRAINT "pk_category" PRIMARY KEY (
         "category_id"
      )
 );
 
 CREATE TABLE "subcategory" (
-    "subcategory_id" varchar10   NOT NULL,
-    "subcategory" varchar64   NOT NULL,
+    "subcategory_id" varchar(10)   NOT NULL,
+    "subcategory" varchar(64)   NOT NULL,
     CONSTRAINT "pk_subcategory" PRIMARY KEY (
         "subcategory_id"
      )
@@ -42,9 +42,9 @@ CREATE TABLE "subcategory" (
 
 CREATE TABLE "contacts" (
     "contact_id" int   NOT NULL,
-    "first_name" varchar25   NOT NULL,
-    "last_name" varchar25   NOT NULL,
-    "email" varchar264   NOT NULL,
+    "first_name" varchar(25)   NOT NULL,
+    "last_name" varchar(25)   NOT NULL,
+    "email" varchar(264)   NOT NULL,
     CONSTRAINT "pk_contacts" PRIMARY KEY (
         "contact_id"
      )
